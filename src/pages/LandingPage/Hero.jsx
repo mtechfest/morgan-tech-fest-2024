14695eaef72f59462784e9e34983a0f65e97e330
mtechfest <144378238+mtechfest@users.noreply.github.com>
2025-09-15 09:38:22 -0400

    Update Hero.jsx

--- src/pages/LandingPage/Hero.jsx
+++ src/pages/LandingPage/Hero.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import judgingImg from '../../assets/rob.png';
+import judgingImg from '../../assets/rob.jpg';
 
 // Container component
 const Container = ({ children, className = "" }) => (
