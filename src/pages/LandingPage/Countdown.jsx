e52060c66ffe41ca4c1a5547eed9a2c122fe4a80
mtechfest <144378238+mtechfest@users.noreply.github.com>
2025-10-02 17:23:06 -0400

    Update Countdown.jsx

--- src/pages/LandingPage/Countdown.jsx
+++ src/pages/LandingPage/Countdown.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 // Container component
 const Container = ({ children, className = "" }) => (
   <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>
