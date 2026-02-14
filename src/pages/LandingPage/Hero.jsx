6d0de9b249f40bc9666491b71768e253133d5779
mtechfest <144378238+mtechfest@users.noreply.github.com>
2026-02-14 18:40:26 -0500

    Update Hero.jsx

--- src/pages/LandingPage/Hero.jsx
+++ src/pages/LandingPage/Hero.jsx
@@ -1,5 +1,4 @@
 import Container from "../../components/Container"
-import NewsSlider from "../../components/NewsSlider" // Add this import
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { FaXTwitter } from "react-icons/fa6";
