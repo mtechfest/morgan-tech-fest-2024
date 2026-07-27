2d0883eb2d52d6b381aec2df478383d3f6536632
depalab <info.favoredonline@gmail.com>
2026-07-27 06:51:10 -0400

    update

--- src/data/home/index.js
+++ src/data/home/index.js
@@ -17,7 +17,7 @@ export const eventInfo = {
 export const nav = [
   { id: 'about', label: 'About', index: '01', href: '/about.html' },
   { id: 'techcase', label: 'Tech Case', href: '/tech-case.html' },
-  { id: 'expo', label: 'Innovation Expo', href: '/innovation-expo.html' },
+  { id: 'expo', label: 'Expo', href: '/innovation-expo.html' },
   { id: 'schedule', label: 'Schedule', index: '04', href: '/schedule.html' },
   { id: 'transit', label: 'Transit', href: '/transit.html' },
   { id: 'sponsors', label: 'Sponsors', index: '05', href: '/sponsors.html' },
